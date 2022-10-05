afef1f7cba6bc26c0debbd0466510c001ecd8da6
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:51:17 +0300

    Added customization for dto`s

---
+++ Test.WebApi/Test.UnitTest/Base/Customization/GenerateValidAccountCustomization.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture;
+using System.Net.Mail;
+using Test.BusinessLogic.Dto.AccountDtos;
+using Test.BusinessLogic.Dto.ContactDtos;
+
+namespace Test.UnitTest.Base.Customization
+{
+    public class GenerateValidAccountCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            //fixture.Customize<CreateIncidentDto>(composer =>
+            //composer
+            //.With(x =>
+            //x.Email,
+            //fixture.Create<MailAddress>().ToString())
+            //.With(x =>
+            //x.AccountName,
+            //fixture.Create<string>())
+            //.With(x =>
+            //x.FirstName,
+            //fixture.Create<string>())
+            //.With(x =>
+            //x.LastName,
+            //fixture.Create<string>())
+            //.With(x =>
+            //x.Description,
+            //fixture.Create<string>()
+            //));
+
+            fixture.Customize<AccountDto>(composer =>
+            composer
+            .With(x =>
+            x.Name,
+            fixture.Create<MailAddress>().ToString())
+            .Without(x =>
+            x.Contacts)
+            );
+
+            fixture.Customize<LinkAccountDto>(composer =>
+            composer
+            .With(x =>
+            x.IncidentName,
+            fixture.Create<string>())
+            .With(x =>
+            x.AccountName,
+            fixture.Create<string>())
+            );
+
+            fixture.Customize<CreateAccountDto>(composer =>
+            composer
+            .With(x =>
+            x.IncidentName,
+            fixture.Create<string>())
+            .With(x =>
+            x.Name,
+            fixture.Create<string>())
+            .With(x =>
+            x.ContactEmail,
+            fixture.Create<MailAddress>().ToString())
+            );
+        }
+    }
+}
