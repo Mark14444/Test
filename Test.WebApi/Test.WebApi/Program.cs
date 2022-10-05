73d57524780e8e1727f35a7360f7e70f43c0226c
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:47:32 +0300

    Update Program.cs

--- Test.WebApi/Test.WebApi/Program.cs
+++ Test.WebApi/Test.WebApi/Program.cs
@@ -1,15 +1,19 @@
+using Test.BusinessLogic.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDb();
+builder.Services.AddDependencies();
+builder.Services.AddValidation();
+builder.Services.AddAutoMapper();
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
