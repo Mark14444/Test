71a53275fb7dce0bbf3545f9eff39e96d771340f
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:44:57 +0300

    Added initial entities

---
+++ Test.WebApi/Test.Domain/Entities/Contact.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Test.Domain.Entities
+{
+    public class Contact
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? AccountName { get; set; }
+        public Account? Account { get; set; }
+    }
+}
