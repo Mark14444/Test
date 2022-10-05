32d7cbb6d0a69c5cbd5e26956d7972eb383e3a4e
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:40:00 +0300

    Create CreateIncidentDto.cs

---
+++ Test.WebApi/Test.BusinessLogic/Dto/IncidentDtos/CreateIncidentDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Test.BusinessLogic.Dto.IncidentDtos
+{
+    public class CreateIncidentDto
+    {
+        public string AccountName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public string? Description { get; set; }
+    }
+}
