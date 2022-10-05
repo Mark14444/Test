7658c56c640c4992ce83405569398e9f81fd5a96
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:41:28 +0300

    Create IncidentProfile.cs

---
+++ Test.WebApi/Test.BusinessLogic/Profiles/IncidentProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Test.BusinessLogic.Dto.IncidentDtos;
+using Test.Domain.Entities;
+
+namespace Test.BusinessLogic.Profiles
+{
+    public class IncidentProfile:Profile
+    {
+        public IncidentProfile()
+        {
+            CreateMap<Incident, CreateIncidentDto>().ReverseMap();
+            CreateMap<Incident, IncidentDto>();
+            CreateMap<Incident, IncidentDto>().ReverseMap();
+        }
+    }
+}
