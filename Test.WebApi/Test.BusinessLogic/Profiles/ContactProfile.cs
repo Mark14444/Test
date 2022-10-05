25488f3b11ecdb6d2d199b571ef8b858c8c60753
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:41:20 +0300

    Added AutoMap profiles

---
+++ Test.WebApi/Test.BusinessLogic/Profiles/ContactProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Test.BusinessLogic.Dto.ContactDtos;
+using Test.BusinessLogic.Dto.IncidentDtos;
+using Test.Domain.Entities;
+
+namespace Test.BusinessLogic.Profiles
+{
+    public class ContactProfile:Profile
+    {
+        public ContactProfile()
+        {
+            CreateMap<Contact, ContactWithAccountDto>().ReverseMap();
+            CreateMap<Contact, CreateIncidentDto>().ReverseMap();
+            CreateMap<Contact, ContactDto>();
+            CreateMap<Contact, ContactDto>().ReverseMap();
+        }
+    }
+}
