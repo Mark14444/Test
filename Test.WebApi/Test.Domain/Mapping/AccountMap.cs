c0c7b09c9ad4fa64a0abba3bb5d525a41ae9b2f4
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:46:25 +0300

    Added relations between tables and constraints

---
+++ Test.WebApi/Test.Domain/Mapping/AccountMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Test.Domain.Entities;
+
+namespace Test.Domain.Mapping
+{
+    public class AccountMap : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(x => x.Name);
+
+            builder.HasMany(x => x.Contacts)
+                .WithOne(y => y.Account)
+                .HasForeignKey(y => y.AccountName);
+        }
+    }
+}
---
+++ Test.WebApi/Test.Domain/Mapping/IncidentMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Test.Domain.Entities;
+
+namespace Test.Domain.Mapping
+{
+    public class IncidentMap : IEntityTypeConfiguration<Incident>
+    {
+        public void Configure(EntityTypeBuilder<Incident> builder)
+        {
+            builder.HasKey(x => x.Name);
+            builder.Property(x => x.Name).ValueGeneratedOnAdd();
+            builder.HasMany(x => x.Accounts)
+                .WithOne(y => y.Incident)
+                .HasForeignKey(y => y.IncidentName);
+        }
+    }
+}
