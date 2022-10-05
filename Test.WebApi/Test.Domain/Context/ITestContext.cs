0b1d3c04d876a6e1d62b47b117c6c4fc475ce6a5
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:44:40 +0300

    Added db context

---
+++ Test.WebApi/Test.Domain/Context/ITestContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Domain.Entities;
+
+namespace Test.Domain.Context
+{
+    public interface ITestContext
+    {
+        DbSet<Incident> Incidents { get; set; }
+        DbSet<Account> Accounts { get; set; }
+        DbSet<Contact> Contacts { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Test.WebApi/Test.Domain/Context/TestContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Test.Domain.Entities;
+using Test.Domain.Mapping;
+
+namespace Test.Domain.Context
+{
+    public class TestContext:DbContext,ITestContext
+    {
+        public TestContext(DbContextOptions options) 
+            : base(options) { }
+
+        public DbSet<Incident> Incidents { get; set; } = null!;
+        public DbSet<Account> Accounts { get; set; } = null!;
+        public DbSet<Contact> Contacts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json");
+                string connectionString = builder.Build().GetConnectionString("conn");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfiguration(new IncidentMap())
+                .ApplyConfiguration(new AccountMap())
+                .ApplyConfiguration(new ContactMap()); 
+            base.OnModelCreating(modelBuilder);
+        }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
