c1dac651f75825708d5d7c4c6188a3476d94ccef
Mark14444 <mih.mark2015@gmail.com>
2022-10-05 10:49:34 +0300

    Create AutoEntityData.cs

---
+++ Test.WebApi/Test.UnitTest/Base/AutoEntityData.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using Test.UnitTest.Base.Customization;
+
+namespace Test.UnitTest.Base
+{
+    public class AutoEntityDataAttribute : AutoDataAttribute
+    {
+#pragma warning disable CS0618
+        public AutoEntityDataAttribute()
+            : base(() => new Fixture().Customize(new AutoMoqCustomization()))
+        {
+            Fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            Fixture.Customize(new GenerateValidContactCustomization());
+            Fixture.Customize(new GenerateValidAccountCustomization());
+            Fixture.Customize(new GenerateValidIncidentCustomization());
+        }
+
+    }
+}
